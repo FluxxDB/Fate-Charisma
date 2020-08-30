f25d0dfc183b16ee5fb222c335ec101f15fa4893
Andrew Rosa <andrewcalderipedarosa@gmail.com>
2020-08-30 18:19:27 -0300

    Fixed Joint

--- src/Client/Controllers/WeaponController.lua
+++ src/Client/Controllers/WeaponController.lua
@@ -5,10 +5,12 @@ local CharacterController
 -- Variables
 local Player = Knit.Player
 
+
 -- Create Knit controller
 local WeaponController = Knit.CreateController {
     Name = "WeaponController";
 }
+
 
 -- Functions
 local function GetOrCreate(Name, Type, Parent)
@@ -21,70 +23,53 @@ local function GetOrCreate(Name, Type, Parent)
 	return Object
 end
 
-local function weldAttachments(attach1, attach2)
-    local weld = Instance.new("Weld")
-    weld.Part0 = attach1.Parent
-    weld.Part1 = attach2.Parent
-    weld.C0 = attach1.CFrame
-    weld.C1 = attach2.CFrame
-    weld.Parent = attach1.Parent
-    return weld
-end
- 
-local function buildWeld(weldName, parent, part0, part1, c0, c1)
-    local weld = Instance.new("Weld")
-    weld.Name = weldName
-    weld.Part0 = part0
-    weld.Part1 = part1
-    weld.C0 = c0
-    weld.C1 = c1
-    weld.Parent = parent
-    return weld
-end
- 
-local function findFirstMatchingAttachment(model, name)
-    for _, child in pairs(model:GetChildren()) do
-        if child:IsA("Attachment") and child.Name == name then
-            return child
-        elseif not child:IsA("Accoutrement") and not child:IsA("Tool") then -- Don't look in hats or tools in the character
-            local foundAttachment = findFirstMatchingAttachment(child, name)
-            if foundAttachment then
-                return foundAttachment
-            end
-        end
-    end
+local function JointAttachments(attach1, attach2)
+    local Joint = Instance.new("Motor6D")
+    Joint.Part0 = attach1.Parent
+    Joint.Part1 = attach2.Parent
+    Joint.C0 = attach1.CFrame
+    Joint.C1 = attach2.CFrame
+    Joint.Parent = attach1.Parent
+    return Joint
 end
 
-local function addWeapon(character, accoutrement)  
-    accoutrement.Parent = character
-    local handle = accoutrement:FindFirstChild("Handle")
-    if handle then
-        local accoutrementAttachment = handle:FindFirstChildOfClass("Attachment")
-        if accoutrementAttachment then
-            local characterAttachment = findFirstMatchingAttachment(character, accoutrementAttachment.Name)
-            if characterAttachment then
-                weldAttachments(characterAttachment, accoutrementAttachment)
-            end
+local function addWeapon(Limb, Weapon)
+    local WeaponAttachment = Weapon:FindFirstChildOfClass("Attachment")
+
+    if WeaponAttachment then
+        local LimbAttachment = Limb:FindFirstChild(WeaponAttachment.Name)
+        if LimbAttachment then
+            JointAttachments(LimbAttachment, WeaponAttachment).Parent = Weapon
         end
     end
 end
 
 local function Equip(Tool, Character)
     local Model = Tool:FindFirstChild("Model")
-    if not (Model or Model:IsA("ObjectValue")) then return end
+    if not (Model and Model:IsA("ObjectValue")) then return end
 
     local WeaponModel = Model.Value
     local ConfigModule = WeaponModel.Parent
-    if not (ConfigModule or ConfigModule:IsA("ModuleScript")) then return end
+    if not (ConfigModule and ConfigModule:IsA("ModuleScript")) then return end
 
     local Config = require(ConfigModule)
-    local Parts = {}
-    for _, LimbName in ipairs(Config.Parts) do
-        local Limb = Character:FindFirstChild(LimbName)
+    local Holder = GetOrCreate("Weapons", "Folder", Character)
+    for Name, Part in ipairs(Config.Parts) do
+        local Limb = Character:FindFirstChild(Name)
         if not Limb then continue end
         
+        local Weapon = Part:Clone()
+        Weapon.Parent = Holder
+        addWeapon(Limb, Weapon)
     end
+
+    local Connection
+    Connection = Tool.AncestryChanged:Connect(function()
+        Holder:Destroy()
+        Connection:Disconnect()
+    end)
 end
+
 
 -- Start
 function WeaponController:KnitStart()
@@ -100,5 +85,6 @@ end
 function WeaponController:KnitInit()
     CharacterController = Knit.Controllers.CharacterController
 end
+
 
 return WeaponController
